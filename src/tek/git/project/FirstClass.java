7894ddedf0d60f91e3e4abf0331a6027e3b8caa1
Abdulrashidhalim <abdulrashidhalim84@gmail.com>
2023-08-16 11:46:58 -0400

    second commit and chaged made to this project

--- src/tek/git/project/FirstClass.java
+++ src/tek/git/project/FirstClass.java
@@ -3,7 +3,7 @@ package tek.git.project;
 public class FirstClass {
 public static void main(String[] args) {
 	
-	System.out.println("hello word");
+	System.out.println("hello word this is my first practic of repository ");
 	
 }
 }
